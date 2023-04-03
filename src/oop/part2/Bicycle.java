dc817ee6cbcec8756b5efb352ab04ac29c7e2d9b
vpm789 <vpm789@gmail.com>
2023-04-03 19:40:45 +0300

    oop2

---
+++ src/oop/part2/Bicycle.java
@@ -0,0 +1,10 @@
+package oop.part2;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
---
+++ src/oop/part2/Truck.java
@@ -0,0 +1,18 @@
+package oop.part2;
+
+public class Truck {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+}
