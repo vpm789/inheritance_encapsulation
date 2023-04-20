3fc9bdad369a8802087eed4dc7142e2f63ac2cff
vpm789 <vpm789@gmail.com>
2023-04-20 22:13:41 +0300

    exceptions

--- src/exceptions/Main.java
+++ src/exceptions/Main.java
@@ -4,11 +4,11 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String login = "vpmльль";
+        String login = "vpm";
         String password = "vpm123";
-        String confirmPassword = "vpm123";
+        String confirmPassword = "vpm12";
         try {
-            checkAccount(login, password, confirmPassword);
+            System.out.println(checkAccount(login, password, confirmPassword));
         } catch (WrongLoginException e) {
             System.out.println("Invalid login, check the right of login");
         } catch (WrongPasswordException e) {
@@ -18,12 +18,13 @@ public class Main {
         }
     }
 
-    static void checkAccount(String login, String password, String confirmPassword) {
+    static Boolean checkAccount(String login, String password, String confirmPassword) {
         if (login.length() > 20 || !login.matches("[a-zA-Z0-9_]+")) {
             throw new WrongLoginException();
-        }
-        if (!password.equals(confirmPassword) || !password.matches("[a-zA-Z0-9_]+")) {
+        } else if (!password.equals(confirmPassword) || !password.matches("[a-zA-Z0-9_]+")) {
             throw new WrongPasswordException();
+        } else {
+            return true;
         }
     }
 
